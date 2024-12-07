9f1483ce151d831cb3d3f696a8fa3c7c38effca8
DarshanBadrakiya <114865157+DarshanBadrakiya@users.noreply.github.com>
2024-12-07 17:47:40 +0530

    Update job.route.js

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -6,11 +6,11 @@ const router = express.Router();
 
 router.route("/post").post(isAuthenticated,postJob);
 
-router.route("/get").get(isAuthenticated,getAllJob);
+router.route("/get").get(getAllJob);
 
 router.route("/getadminjob").get(isAuthenticated,getAdminJobs);
 
-router.route("/get/:id").get(isAuthenticated,getJobById);
+router.route("/get/:id").get(getJobById);
 
 
 export default router;
