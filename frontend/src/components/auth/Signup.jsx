480bd0ad7df6f2873279a8e59e58bd247ef0335a
DarshanBadrakiya <114865157+DarshanBadrakiya@users.noreply.github.com>
2024-12-07 22:18:27 +0530

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../shared/Navbar";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup } from "@/components/ui/radio-group";
 import { Button } from "../ui/button";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -12,43 +12,46 @@ import { useDispatch, useSelector } from "react-redux";
 import store from "@/redux/store";
 import { setLoading } from "@/redux/authSlice";
 import { Loader2 } from "lucide-react";
+
 const Signup = () => {
-    const [input,setInput] = useState({
-        fullname:"",
-        email:"",
-        phoneNumber:"",
-        password:"",
-        role:"",
-        file:""
+    const [input, setInput] = useState({
+        fullname: "",
+        email: "",
+        phoneNumber: "",
+        password: "",
+        role: "",
+        file: ""
     });
-    const {loading,user} = useSelector(store=>store.auth);
+    const { loading, user } = useSelector(store => store.auth);
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const changeEventHandler = (e) => {
-        setInput({...input, [e.target.name]:e.target.value});
 
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value });
     }
+
     const changeFileHandler = (e) => {
-        setInput({...input,file:e.target.files?.[0]});
+        setInput({ ...input, file: e.target.files?.[0] });
     }
-    const submitHandler =  async (e) => {
+
+    const submitHandler = async (e) => {
         e.preventDefault();
         const formData = new FormData();
-        formData.append("fullname",input.fullname);
-        formData.append("email",input.email);
-        formData.append("phoneNumber",input.phoneNumber);
-        formData.append("password",input.password);
-        formData.append("role",input.role);
-        if(input.file){
-            formData.append("file",input.file);
+        formData.append("fullname", input.fullname);
+        formData.append("email", input.email);
+        formData.append("phoneNumber", input.phoneNumber);
+        formData.append("password", input.password);
+        formData.append("role", input.role);
+        if (input.file) {
+            formData.append("file", input.file);
         }
         try {
             dispatch(setLoading(true));
-            const res = await axios.post(`${USER_API_END_POINT}/register` , formData , {
-                headers:{'Content-Type':"multipart/form-data"},
-                withCredentials:true
+            const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+                headers: { 'Content-Type': "multipart/form-data" },
+                withCredentials: true
             });
-            if(res.data.success){
+            if (res.data.success) {
                 navigate("/login");
                 toast.success(res.data.message);
             }
@@ -59,20 +62,22 @@ const Signup = () => {
             } else {
                 toast.error('Error Message:', error.message);
             }
-        }finally{
+        } finally {
             dispatch(setLoading(false))
         }
     }
-    useEffect(()=>{
-        if(user){
+
+    useEffect(() => {
+        if (user) {
             navigate("/home")
         }
     })
+
     return (
         <div>
             <Navbar />
-            <div className="flex items-center justify-center max-w-7xl mx-auto">
-                <form onSubmit={submitHandler} className="w-1/2 border border-grey-200 rounded-md p-4 my-10">
+            <div className="flex items-center justify-center max-w-7xl mx-auto px-4">
+                <form onSubmit={submitHandler} className="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 border border-gray-200 rounded-md p-4 my-10">
                     <h1 className="font-bold text-xl mb-5">Sign Up</h1>
                     <div className="my-2">
                         <Label>Full Name</Label>
@@ -93,21 +98,21 @@ const Signup = () => {
                     <div className="flex items-center justify-between">
                         <RadioGroup className="flex items-center gap-4 my-5">
                             <div className="flex items-center space-x-2">
-                                <Input type="radio" name="role" value="Candidate" checked={input.role == 'Candidate'} onChange={changeEventHandler} className="cursor-pointer"/>
+                                <Input type="radio" name="role" value="Candidate" checked={input.role == 'Candidate'} onChange={changeEventHandler} className="cursor-pointer" />
                                 <Label htmlFor="option-one">Candidate</Label>
                             </div>
                             <div className="flex items-center space-x-2">
-                            <Input type="radio" name="role" value="Recruiter" checked={input.role == 'Recruiter'} onChange={changeEventHandler} className="cursor-pointer"/>
-                            <Label htmlFor="option-two">Recruiter</Label>
+                                <Input type="radio" name="role" value="Recruiter" checked={input.role == 'Recruiter'} onChange={changeEventHandler} className="cursor-pointer" />
+                                <Label htmlFor="option-two">Recruiter</Label>
                             </div>
                         </RadioGroup>
                         <div className="flex items-center ">
                             <Label>Profile</Label>
-                            <Input accept="image/*" onChange={changeFileHandler} type="file" className="cursor-pointer"/>
+                            <Input accept="image/*" onChange={changeFileHandler} type="file" className="cursor-pointer" />
                         </div>
                     </div>
                     {
-                        loading ? <Button className="w-full my-4"><Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please Wait</Button> :                    <Button className="w-full my-4" type="submit">Signup</Button>
+                        loading ? <Button className="w-full my-4"><Loader2 className="mr-2 h-4 w-4 animate-spin" />Please Wait</Button> : <Button className="w-full my-4" type="submit">Signup</Button>
                     }
                     <span className="text-sm">Already have an account? <Link to="/login" className="text-blue-600">Login</Link></span>
                 </form>
