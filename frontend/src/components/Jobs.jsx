63cb304a476e322de04ea4951c81d92d1a9cc6d3
DarshanBadrakiya <114865157+DarshanBadrakiya@users.noreply.github.com>
2024-12-07 22:10:26 +0530

    Update Jobs.jsx

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './shared/Navbar'
-import FilterCard from './FilterCard'
+import React, { useEffect, useState } from 'react';
+import Navbar from './shared/Navbar';
 import Job from './Job';
 import { useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
-
-// const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const Jobs = () => {
     const { allJobs, searchedQuery } = useSelector(store => store.job);
@@ -17,10 +14,10 @@ const Jobs = () => {
                 return job.title.toLowerCase().includes(searchedQuery.toLowerCase()) ||
                     job.description.toLowerCase().includes(searchedQuery.toLowerCase()) ||
                     job.location.toLowerCase().includes(searchedQuery.toLowerCase())
-            })
-            setFilterJobs(filteredJobs)
+            });
+            setFilterJobs(filteredJobs);
         } else {
-            setFilterJobs(allJobs)
+            setFilterJobs(allJobs);
         }
     }, [allJobs, searchedQuery]);
 
@@ -29,13 +26,10 @@ const Jobs = () => {
             <Navbar />
             <div className='max-w-7xl mx-auto mt-5'>
                 <div className='flex gap-5'>
-                    {/* <div className='w-20%'>
-                        <FilterCard />
-                    </div> */}
                     {
                         filterJobs.length <= 0 ? <span>Job not found</span> : (
                             <div className='flex-1 h-[88vh] overflow-y-auto pb-5'>
-                                <div className='grid grid-cols-3 gap-4'>
+                                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                                     {
                                         filterJobs.map((job) => (
                                             <motion.div
@@ -54,10 +48,8 @@ const Jobs = () => {
                     }
                 </div>
             </div>
-
-
         </div>
-    )
+    );
 }
 
-export default Jobs
+export default Jobs;
