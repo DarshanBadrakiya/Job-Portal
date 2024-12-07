cf57d855f22bd8198b23be621391e625e2e136a9
DarshanBadrakiya <114865157+DarshanBadrakiya@users.noreply.github.com>
2024-12-07 16:28:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin : 'https://job-portal-frontend-3vpq.onrender.com/',
+    origin : 'https://job-portal-frontend-3vpq.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials:true 
 } 
