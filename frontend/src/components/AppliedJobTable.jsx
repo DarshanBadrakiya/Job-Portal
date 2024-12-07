0e7911e7d7790514f1e4e235d592f766b5ae90d3
DarshanBadrakiya <114865157+DarshanBadrakiya@users.noreply.github.com>
2024-12-07 22:16:15 +0530

    Update AppliedJobTable.jsx

--- frontend/src/components/AppliedJobTable.jsx
+++ frontend/src/components/AppliedJobTable.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
 import { Badge } from "./ui/badge";
 import { useSelector } from "react-redux";
-import store from "@/redux/store";
 
 const AppliedJobTable = () => {
-    const {allAppliedJobs} = useSelector(store=>store.job);
+    const { allAppliedJobs } = useSelector(store => store.job);
     return (
         <div>
             <Table>
@@ -20,14 +19,19 @@ const AppliedJobTable = () => {
                 </TableHeader>
                 <TableBody>
                     {
-                        allAppliedJobs?.length <= 0 ? <span>You Haven't Applied any Job yet</span> : allAppliedJobs?.map((appliedJob,index) => (
+                        allAppliedJobs?.length <= 0 ? <span>You Haven't Applied to Any Job Yet</span> : allAppliedJobs?.map((appliedJob, index) => (
                             <TableRow key={appliedJob?._id}>
                                 <TableCell>{appliedJob?.createdAt.split("T")[0]}</TableCell>
                                 <TableCell>{appliedJob?.job?.title}</TableCell>
                                 <TableCell>{appliedJob?.job?.company?.name}</TableCell>
-                                <TableCell className="text-right"><Badge className={
-                                    `${appliedJob?.status==='rejected'?'bg-red-400': appliedJob.status === 'pending'?'bg-gray-400':'bg-green-400'}`
-                                }>{appliedJob?.status==='rejected'?'Rejected': appliedJob.status === 'pending'?'Pending':'Accepted'}</Badge></TableCell>
+                                <TableCell className="text-right">
+                                    <Badge className={`
+                                        ${appliedJob?.status === 'rejected' ? 'bg-red-400' :
+                                            appliedJob.status === 'pending' ? 'bg-gray-400' : 'bg-green-400'}`}>
+                                        {appliedJob?.status === 'rejected' ? 'Rejected' : 
+                                            appliedJob.status === 'pending' ? 'Pending' : 'Accepted'}
+                                    </Badge>
+                                </TableCell>
                             </TableRow>
                         ))
                     }
